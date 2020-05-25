5fbf5595c677848e006b25d4905d2e8a3352b9fd
juliorivaldo <62598675+juliorivaldo@users.noreply.github.com>
2020-05-25 14:15:37 +0700

    Add files via upload

---
+++ Final_Project_smt2.c
@@ -0,0 +1,12 @@
+/*
+* Final Project smt 2 --- 
+* Topic: Red Black Tree
+* Written by : Putri Aurelia Shilo, Vincentius Sutanto, Julio Rivaldo Gunawan
+* NIM : 2301878952, 2301879583, 2301943491
+* Title : Input Food Code using Red Black Tree Implementaion
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
